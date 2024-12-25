bf29422ec54ddd862ce91e14ea4a6f31ac5cca34
skg1998 <sahilkg1998@gmail.com>
2024-12-25 13:26:43 +0530

    init setup

---
+++ api/src/test/java/com/stayease/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stayease.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
