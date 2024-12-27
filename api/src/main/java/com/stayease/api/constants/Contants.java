1638c990aa8a4908bfffaaab76c2fe1f16da679b
skg1998 <sahilkg1998@gmail.com>
2024-12-27 12:12:59 +0530

    setup constants

---
+++ api/src/main/java/com/stayease/api/constants/Contants.java
@@ -0,0 +1,9 @@
+package com.stayease.api.constants;
+
+public final class Contants {
+	public static final String SECURITY_SCHEME_NAME = "bearerAuth";
+	public static final String DATE_FORMAT = "yyyy-MM-dd";
+	public static final String DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+	public static final String TOKEN_HEADER = "Authorization";
+	public static final String TOKEN_TYPE = "Bearer";
+}
